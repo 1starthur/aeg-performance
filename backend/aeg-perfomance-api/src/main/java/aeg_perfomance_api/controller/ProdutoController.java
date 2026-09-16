25ef45ab3e4f287c20f346d401ba7f04b3ecb4bb
Arthur Nery <arthurgabrielnery@gmail.com>
2026-09-16 12:26:18 -0300

    feat: retornar lista de produtos na API

--- backend/aeg-perfomance-api/src/main/java/aeg_perfomance_api/controller/ProdutoController.java
+++ backend/aeg-perfomance-api/src/main/java/aeg_perfomance_api/controller/ProdutoController.java
@@ -4,15 +4,31 @@ import aeg_perfomance_api.model.Produto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class ProdutoController {
 
     @GetMapping("/produtos")
-    public Produto listarProdutos(){
-        Produto produto = new Produto();
-
-        return produto;
-
-
+    public List<Produto> listarProdutos(){
+        Produto produto1 = new Produto(
+                1L,
+                "Pastilha de freio",
+                149.90,
+                10
+        );
+        Produto produto2 = new Produto(
+                2L,
+                "Amortecedor",
+                389.90,
+                5
+        );
+        Produto produto3 = new Produto(
+                3L,
+                "Filtro de óleo",
+                39.90,
+                20
+        );
+        return List.of(produto1,produto2,produto3);
     }
 }
--- backend/aeg-perfomance-api/src/main/java/aeg_perfomance_api/model/Produto.java
+++ backend/aeg-perfomance-api/src/main/java/aeg_perfomance_api/model/Produto.java
@@ -6,4 +6,25 @@ public class Produto {
     private String nome;
     private double preco;
     private int estoque;
+
+    public Produto(long id,String nome, double preco, int estoque){
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+        this.estoque = estoque;
+    }
+
+    public long getId(){
+        return id;
+    }
+    public String getNome(){
+        return nome;
+    }
+    public double getPreco(){
+        return preco;
+    }
+
+    public int getEstoque() {
+        return estoque;
+    }
 }
